3a6b63c5c9622019b4a774bd234c8b14ac3de2c7
justha <nganha42@gmail.com>
2020-07-31 11:32:06 -0500

    styled containers--search, notes form

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -2,8 +2,8 @@ const contentTarget = document.querySelector(".container__noteForm")
 
 const render = () => {
 contentTarget.innerHTML = `
+<h3>Notes Form</h3>
 <form id="noteForm">
-    <div><b>Notes Form</b></div>
     <input type="text" id="noteForm--author" placeholder="enter your name"></input>
     <input type="text" id="noteForm--suspect" placeholder="possible suspect(s)"></input>
     <textarea id="noteForm--comment" placeholder="comments"></textarea>
