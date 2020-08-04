5d7735a5c25d6cab4b9c7b7f78f950c5f3c7b77e
justha <nganha42@gmail.com>
2020-08-04 11:12:11 -0500

    add alibi button to CriminalHtml.js

--- scripts/criminals/criminalHtml.js
+++ scripts/criminals/criminalHtml.js
@@ -4,11 +4,13 @@ export const criminalHtml = (criminalObj) => {
     return `
         <section class="criminal__card">
             <div class="criminal__name">${criminalObj.name}</div>
-            <div><b>Age: </b>${criminalObj.age}</div>
-            <div><b>Crime: </b>${criminalObj.conviction}</div>
-            <div><b>Term Start: </b>${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</div>
-            <div><b>Term End: </b>${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</div>
-            <div><b>Arresting Officer: </b>${criminalObj.arrestingOfficer}</div>
-            </section>
+            <divclass="criminal__age><b>Age: </b>${criminalObj.age}</div>
+            <div class="criminal__crime"><b>Crime: </b>${criminalObj.conviction}</div>
+            <div class="criminal__termStart"><b>Term Start: </b>${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</div>
+            <div class="criminal__termEnd"><b>Term End: </b>${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</div>
+            <div class="criminal__officer"><b>Arresting Officer: </b>${criminalObj.arrestingOfficer}</div>
+
+            <button class="criminal__alibiButton" id="${criminalObj.id}">Associate Alibi</button>
+        </section>
     `
 }
