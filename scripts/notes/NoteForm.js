da883e9db736c464047767dd9c870f01a8f29c76
justha <nganha42@gmail.com>
2020-08-14 12:16:45 -0500

    note form now has suspect selector with criminal names in dropdown

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,11 +1,12 @@
-import { saveNote } from "./NoteProvider.js";
+import { saveNote, useNotes } from "./NoteProvider.js";
+import { getCriminals, useCriminals } from "../criminals/CriminalProvider.js";
 
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".container__noteForm")
 
 // event listener -- hear "noteSaved" click event
 eventHub.addEventListener("click", (clickEvent) => {
-    if (clickEvent.target.id === "noteForm--saveNote") {
+    if (clickEvent.target.id === "noteForm--saveButton") {
         const noteAuthor = document.querySelector("#noteForm--author")
         const noteSuspect = document.querySelector("#noteForm--suspect")
         const noteComment = document.querySelector("#noteForm--comment")
@@ -21,20 +22,35 @@ eventHub.addEventListener("click", (clickEvent) => {
 })
 
 
-// NOTE--button needs to reside outside of the <form> tags 
-const render = () => {
-contentTarget.innerHTML = `
-<h3>Enter Your Notes Here</h3>
-<form id="noteForm">
-    <input type="text" id="noteForm--author" placeholder="enter your name"></input>
-    <input type="text" id="noteForm--suspect" placeholder="possible suspect(s)"></input>
-    <textarea id="noteForm--comment" placeholder="comments here"></textarea>
-</form>
-<button id="noteForm--saveNote">Save Note</button>
-`
+// NOTE--button has to reside OUTSIDE of <form> tags
+// note form with input fields AND drop-down option selector
+const render = (criminals) => {
+    contentTarget.innerHTML = `
+    <h3>Enter Your Notes Here</h3>
+    <form id="noteForm">
+        <input type="text" id="noteForm--author" placeholder="enter your name..."></input>
+        
+        <select id="noteForm--suspect" placeholder="possible suspect">
+            <option value="0">select a criminal...</option>
+            ${
+                criminals.map(criminalObj => {
+                return `<option value="${criminalObj.id}">${criminalObj.name}</option>`
+                })
+            }
+        </select>
+        
+        <textarea id="noteForm--comment" placeholder="comments here..."></textarea>
+    </form>
+
+    <button id="noteForm--saveButton">Save</button>
+    `
 }
 
 
 export const noteForm = () => {
-    render()
+    getCriminals()
+    .then (()=> {
+        const criminals = useCriminals()
+        render(criminals)
+    })
 }
--- scripts/notes/NoteHtml.js
+++ scripts/notes/NoteHtml.js
@@ -1,8 +1,8 @@
-export const noteHtml = (noteObj) => {
+export const noteHtml = (noteObj, criminalObj) => {
     return `
         <section class="note__card">
             <div class="note__author"><b>Author: </b>${noteObj.author}</div>
-            <div class="note__suspect"><b>Possible Suspect(s): </b>${noteObj.suspect}</div>
+            <div class="note__suspect"><b>Possible Suspect: </b>${criminalObj.name}</div>
             <div class="note__comment"><b>Comments: </b>${noteObj.comment}</div>
             <div class="note__timestamp"><b>Timestamp: </b>${new Date(noteObj.timestamp).toLocaleDateString('en-US')}</div>
         </section>
