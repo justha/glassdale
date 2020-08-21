a66520c052418752dc197b27a3f72b6808e5cb0f
justha <nganha42@gmail.com>
2020-08-21 11:45:54 -0500

    related facilities now listed by criminal

--- scripts/criminals/criminalHtml.js
+++ scripts/criminals/criminalHtml.js
@@ -3,15 +3,17 @@ const contentTarget = document.querySelector(".container__criminals")
 
 
 // Criminal card: name, age, conviction, dates of incarceration
-export const criminalHtml = (criminalObj) => {
+export const criminalHtml = (criminalObj, facilities) => {
     return `
         <section class="criminal__card">
-            <div class="criminal__name">${criminalObj.name}</div>
+            <h3 class="criminal__name">${criminalObj.name}</h3>
             <divclass="criminal__age><b>Age: </b>${criminalObj.age}</div>
             <div class="criminal__crime"><b>Crime: </b>${criminalObj.conviction}</div>
             <div class="criminal__termStart"><b>Term Start: </b>${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</div>
             <div class="criminal__termEnd"><b>Term End: </b>${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</div>
             <div class="criminal__officer"><b>Arresting Officer: </b>${criminalObj.arrestingOfficer}</div>
+            <div class="criminal__facility"><b>Facilities: </b></div>
+                <ul>${facilities.map(facility => `<li>${facility.facilityName}</li>`).join("")}</ul>
 
             <button class="criminal__alibiButton" id="criminal__alibiButton--${criminalObj.id}">Alibi</button>
         </section>
